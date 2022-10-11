6255d06c2f142010656c92224f57ce1def98b6ec
claude-at-216593087750 <ducharmeclaude831@yahoo.com>
2022-10-11 10:01:05 +0800

    Add StreamId and State

--- start-docker-envvars.sh
+++ start-docker-envvars.sh
@@ -5,4 +5,4 @@ export REDIS_HOST=$(grep REDIS_HOST $ENV_FILE | cut -d '=' -f2)
 export AWS_REGION=$(grep AWS_REGION $ENV_FILE | cut -d '=' -f2)
 export SQS_QUEUE_URL=$(grep SQS_QUEUE_URL $ENV_FILE | cut -d '=' -f2)
 
-sudo docker run -it sqsdeviceconsumer-toredisearch:v0.01 --env REDIS_HOST=$REDIS_HOST AWS_REGION=$AWS_REGION SQS_QUEUE_URL=$SQS_QUEUE_URL
+sudo docker run -it sqsdeviceconsumer-toredisearch:v0.02 --env REDIS_HOST=$REDIS_HOST AWS_REGION=$AWS_REGION SQS_QUEUE_URL=$SQS_QUEUE_URL
